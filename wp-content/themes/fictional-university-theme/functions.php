4b6be47435b5fd12114096081e718835c60d25c7
GodwinYeboah <godwinyeboah95@gmail.com>
2019-03-06 18:46:11 -0500

    adding header and footer structure

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php 
 // load files here
 function university_files(){
+    wp_enqueue_style('font-awesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"');
     wp_enqueue_style('university_main_styles',get_stylesheet_uri());
     
 }
